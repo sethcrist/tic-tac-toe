adf90da716012fb46bc2d8b0bacb52d7a7515774
Seth Crist <sethcrist50@gmail.com>
2024-02-28 12:10:02 -0600

    added a function to index the click

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,40 @@
+import {useState} from "react";
 import './App.css';
+import Square from "./components/Square";
 
-function App() {
+
+const App = () => {
+    const [board, setBoard] = useState([
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?",
+        "?"
+    ])
+
+    const handleGamePlay = (currentSquare) => {
+        alert(currentSquare)
+    }
+
   return (
       <>
-
+        <h1>Treasure Hunt Game</h1>
+          <div className="board">
+              {board.map((square, index) => {
+                  return (
+                      <Square
+                      square={square}
+                      key={index}
+                      index={index}
+                      handleGamePlay={handleGamePlay}
+                      />
+                  )
+              })}
+          </div>
       </>
   )
 }
---
+++ my-app/src/components/Square.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Square = (props) => {
+
+    const handleClick = () => {
+        props.handleGamePlay(props.index)
+    }
+
+    return (
+     <>
+         <div className="square" onClick={handleClick}>
+             {props.square}
+         </div>
+    </>
+    )
+}
+export default Square
